bb68d56c2b386543a035abc9cbf10671e735f2c2
vishal <vishal.patel929@gmail.com>
2020-06-10 21:24:28 -0400

    Almost done with logic. Only thing left is to setup result display

--- calendarLogic.py
+++ calendarLogic.py
@@ -110,22 +110,6 @@ def getInitialDateRange():
     else :
         ttk.Button(top,text="confirm",command=setEndDate).pack()
     
-def getIndividualDate():
-    top =tk.Toplevel(root)
-    ttk.Label(top,text='Choose date').pack(padx=10,pady=10)
-
-    mindate=datetime.date.today()
-    #exception handling
-
-    try:
-        maxdate=datetime.date(mindate.year+4,mindate.month,mindate.day)
-    except:
-        #then we were on a leap year on last day of Feb
-        maxdate=datetime.date(mindate.year+4,mindate.month+1,1)
-
-    cal=DateEntry(top,width=12,background='darkblue',foreground='white',borderwidth=2,mindate=mindate,maxdate=maxdate)
-    cal.pack(padx=10,pady=10)
-    ttk.Button(top,text="confirm").pack()
 
 def setupPersonSelection():
     def moveToAvailability():
@@ -142,23 +126,141 @@ def setupPersonSelection():
             for widget in root.winfo_children():
                 widget.destroy()    
             setupDateSelection()
+    def moveToResult():
+        #destroy current widgets and make the screen a result screen
+        for widget in root.winfo_children():
+            widget.destroy()    
+        setupResultView()
     label=ttk.Label(root,text="Participant Selection").pack()
     nameText=ttk.Entry(root,text="Enter Name").pack()
     availabilityButton = ttk.Button(root,text="Set Availablity for This Participant",command=moveToAvailability).pack(padx=10,pady=10)
     if (personEntered!=None):
         #then we need to add an extra option to stop person entering
-        moveToResultsButton=ttk.Button(root,text="Stop entering Persons").pack(padx=10,pady=10)
+        moveToResultsButton=ttk.Button(root,text="Stop entering Persons",command=moveToResult).pack(padx=10,pady=10)
 
 def setupDateSelection():
+    isDestroyed=False
+    dateEntered=None
+
+    def moveToResultView():
+        #basically destroying the root and getting the result screen
+        for widget in root.winfo_children():
+            widget.destroy()
+        setupResultView()
+
+    def setStopDatesButton():
+        stopDateButton=ttk.Button(root,text="Stop entering dates",command=moveToResultView).pack()
+
+    def destroyAvailables():
+        #function to destroy "all available" and "all unavailable" options once a user enters a date or date range
+        if (!isDestroyed):
+            allAvailableButton.destroy
+            allAbsentButton.destroy
+            isDestroyed=True
+
+
+    def setAbsent():
+        global graph
+        graph.addAbsentUser(personEntered)
+        for widget in root.winfo_children():
+            widget.destroy()
+        setupPersonSelection()
+    def setAvailable():
+        #available doesnt really do anything, we just move on to the next user
+        for widget in root.winfo_children():
+            widget.destroy()
+        setupPersonSelection()
+    
+    def selectIndividualDate():
+        def confirmIndividualDate():
+            dateEntered=cal.get_date()
+            #now I add the user/date pair to the graph and remove the window
+            global graph
+            graph.addUserDate(personEntered,dateEntered)
+            destroyAvailables()
+            top.destroy()
+
+        top =tk.Toplevel(root)
+        ttk.Label(top,text='Choose a date').pack(padx=10,pady=10)
+
+        mindate=datetime.date.today()
+        #exception handling
+
+        try:
+            maxdate=datetime.date(mindate.year+4,mindate.month,mindate.day)
+        except:
+            #then we were on a leap year on last day of Feb
+            maxdate=datetime.date(mindate.year+4,mindate.month+1,1)
+
+        cal=DateEntry(top,width=12,background='darkblue',foreground='white',borderwidth=2,mindate=mindate,maxdate=maxdate)
+        cal.pack(padx=10,pady=10)
+        ttk.Button(top,text="confirm",command=confirmIndividualDate).pack()
+  
+    def getDateRange():
+        lowDate=None
+        highDate=None
+        def setLowDate():
+            lowDate=cal.get_date()
+            top.destroy()
+            getDateRange()
+
+        def setHighDate():
+            highDate=cal.get_date()
+            #for I/O removal of buttons
+            dateEntered=highDate
+            #destroying current window
+            top.destroy()
+            #now we can add all the dates in this range to the graph
+            dates=getDateRange(lowDate,highDate)
+            #adding to graph
+            global graph
+            global personEntered
+            for x in dates:
+                graph.addUserDate(personEntered,x)
+                
+
+        top =tk.Toplevel(root)
+        if (lowDate==None):
+            ttk.Label(top,text='Choose the first possible date for your event!').pack(padx=10,pady=10)
+        else :
+            ttk.Label(top,text='Choose the last possible date for your event!').pack(padx=10,pady=10)
+
+        if (lowDate==None):
+            global begDate
+            mindate=begDate
+        else:
+            mindate=lowDate
+        #exception handling
+
+        try:
+            maxdate=datetime.date(mindate.year+4,mindate.month,mindate.day)
+        except:
+            #then we were on a leap year on last day of Feb
+            maxdate=datetime.date(mindate.year+4,mindate.month+1,1)
+
+        cal=DateEntry(top,width=12,background='darkblue',foreground='white',borderwidth=2,mindate=mindate,maxdate=maxdate)
+        cal.pack(padx=10,pady=10) 
+        if (lowDate==None):
+            ttk.Button(top,text="confirm",command=setLowDate).pack()
+        else :
+            ttk.Button(top,text="confirm",command=setHighDate).pack()
+
+
+
+
+
+
     #now we attach some widget buttons to the root
     global personEntered
-    label=ttk.Label(root,text="Select Availability For "+personEntered)
-    allAvailableButton= ttk.Button(root,text="Available on Every Day").pack()
-    allAbsentButton=ttk.Button(root,text="Unavailable on Every Day").pack()
-    selectDateRangeButton=ttk.Button(root,text="Select Date Range").pack()
-    selectIndividualDateButton=ttk.Button(root,text="Select Individual Date").pack()
+    label=ttk.Label(root,text="Select Availability For "+personEntered).pack()
+    allAvailableButton= ttk.Button(root,text="Available on Every Day",command=setAvailable).pack()
+    allAbsentButton=ttk.Button(root,text="Unavailable on Every Day",command=setAbsent).pack()
+    selectDateRangeButton=ttk.Button(root,text="Select Date Range where the individual is absent",command=selectDateRange).pack()
+    selectIndividualDateButton=ttk.Button(root,text="Select Individual Date where the individual is absent",command=selectIndividualDate).pack()
+    stopDatesButton=None
 
-
+def setupResultView():
+    label=ttk.Label(root,text="Best Dates for the Event").pack()
 
 root=tk.Tk()
 
